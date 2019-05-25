35d3d5266c6fc4b974cc595ceca85e7be041f99b
Krithika Raghavendran <krithika.ragha@gmail.com>
2019-05-25 16:13:11 -0400

    Update tectonicplates.js

--- Basic/static/js/tectonicplates.js
+++ Basic/static/js/tectonicplates.js
@@ -4,7 +4,7 @@ var myMap = L.map("map", {
   zoom: 4
 });
 
-// Define an outdoors tile layer to the map
+// Define an outdoors tile layer for the map
 var outdoormap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
